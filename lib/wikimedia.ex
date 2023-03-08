7ae31f66468f0fa8ab03e306db4f1c5d3cd26885
Josue Velez <josuevelezdr@gmail.com>
2023-03-08 22:47:10 +0100

    feat: Defining supervisor

---
+++ lib/wikimedia.ex
@@ -0,0 +1,18 @@
+defmodule Wikimedia do
+  @moduledoc false
+  use Application
+
+  def start(_type, _args) do
+    IO.inspect("Starting supervisor...")
+
+    children = [
+        Avrora,
+        Wikimedia.ChangeHandler,
+        Wikimedia.Consumer
+      ]
+
+    opts = [strategy: :one_for_one, name: Wikimedia.Supervisor]
+
+    Supervisor.start_link(children, opts)
+  end
+end
