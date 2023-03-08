a83423eb69447be5f7c402f6d1c1c20817918b01
Josue Velez <josuevelezdr@gmail.com>
2023-03-08 18:06:28 +0100

    feat: Adding configuration for avro

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,18 @@
 import Config
 
+config :avrora,
+  # otp_app: :my_application, # optional, if you want to use it as a root folder for `schemas_path`
+  registry_url: "http://localhost:8081",
+  # registry_auth: {:basic, ["username", "password"]}, # optional
+  registry_user_agent: "Avrora/0.24.2 Elixir", # optional: if you want to return previous behaviour, set it to `nil`
+  schemas_path: "./lib/wikimedia/schemas",
+  registry_schemas_autoreg: false, # optional: if you want manually register schemas
+  convert_null_values: false, # optional: if you want to keep decoded `:null` values as is
+  convert_map_to_proplist: false, # optional: if you want to restore the old behavior for decoding map-type
+  names_cache_ttl: :timer.minutes(5) # optional: if you want periodic disk reads
+  # decoder_hook: &MyClient.decoder_hook/4 # optional: if you want to amend the data/result
+
+
 config :kafka_ex,
   # A list of brokers to connect to. This can be in either of the following formats
   #
--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule KafkaBeginner.MixProject do
     [
       {:kafka_ex, "~> 0.13.0"},
       {:jason, "~> 1.3"},
-      {:httpoison, "~> 2.0"}
+      {:httpoison, "~> 2.0"},
+      {:avrora, "~> 0.21"}
 
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
